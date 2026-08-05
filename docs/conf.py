17cd602046d755d4f68b374cabbb74ef7a0a787e
Turtlerock0010 <danielrprincipe@gmail.com>
2026-08-04 21:06:23 -0400

    Initialize Sphinx documentation configuration

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = "Carbonara_DRV8874"
+copyright = "2026, Carbonara Individuals"
+author = "Carbonara Individuals"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
