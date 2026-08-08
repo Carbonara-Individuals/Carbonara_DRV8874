5455dd677e2845f3513ceb0ea746c01dc562bb84
Turtlerock0010 <danielrprincipe@gmail.com>
2026-08-08 13:28:24 -0400

    Create Compile_Time_Checker.ino for library checks
    Added a simple script for library compile-time checks.

---
+++ Compile_Time_Checker/Compile_Time_Checker.ino
@@ -0,0 +1,14 @@
+/*
+This is a simple script used for library checks to see if it runs during compile time.
+Despite it's use as a tool for the backend, feel free to use it however you like!
+*/
+#include <Carbonara_DRV8874.h>
+
+
+void setup() {
+  // Put your code here
+}
+
+void loop() {
+  // Put your code here
+}
